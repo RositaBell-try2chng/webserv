e7d5774c1009f99b41233ea505799e8d9873a9ae
Sprouts Coach <scoach@c2r11s6.42yerevan.am>
2023-05-08 16:12:11 +0400

    html_parser base

---
+++ request_parse/request_parse.cpp
@@ -0,0 +1,27 @@
+#include "Request_parse.hpp"
+
+Request_parse::HTTP_Request() {};
+Request_parse::~HTTP_Request() {};
+
+HTTP_Request Request_parse::strtoreq(std::string raw) {
+
+	HTTP_Request req(); 
+
+	for (int i == 0; raw[i] != ' '; ++i)
+		req->method.push_back(raw[i]);
+	raw.erase(0, i);
+	
+	for (int i == 0; raw[i] != ' '; ++i)
+		req->uri.push_back(raw[i]);
+	raw.erase(0, i);
+
+	if (raw.substr(0, 5).compare("HTML/"))
+		raw.erase(0, 5);
+	
+	for (int i == 0; raw[i] != '\n'; ++i)
+		if (raw[i] != '\r')
+			req->uri.push_back(raw[i]);
+	raw.erase(0, i);
+
+
+};
--- request_parse/request_parse.hpp
+++ request_parse/request_parse.hpp
@@ -3,6 +3,30 @@
 
 # include "../webserv.hpp"
 
+struct HTTP_Request
+{
+	private:
+
+		HTTP_Request();
+		~HTTP_Request();
+
+	public:
+
+		std::string							method;
+		std::string							uri;
+		std::string							version;
+
+		std::map<std::string, std::string>	headers;
+		
+		std::string							body;
+
+	// Get "raw" request and parse into structure
+		HTTP_Request ft_strtoreq(std::string raw);
+
+		
+		
+};
+
 
 
 #endif
