2c0ed20d2e6356e72007595f5fd0994c21bddae2
scoach <scoach@student.21-school.ru>
2023-05-26 17:16:27 +0400

    New parsing: completed

--- request_parse/HTTP_Request.cpp
+++ request_parse/HTTP_Request.cpp
@@ -1,73 +1,206 @@
 #include "HTTP_Request.hpp"
 
-// void ft_tokenisation(std::string raw) {
+std::vector<std::string> ft_tokenisation(std::string raw) {
 
-// 	int end = raw.size() - 1;
-// 	std::string *strs;
-// 	int str_nmbr = 0;
-// 	int	letter;
+	int end = raw.size() - 1;
+	std::vector<std::string> strs;
 
-// 	for (int i = 0; raw[i] != end; ++i) {
-// 		letter = 0;
-// 		while (raw[i + letter] != '\n' && raw[i + letter - 1] != '\r')
-// 			letter++;
-// 		strs[str_nmbr].append(raw.substr(i, letter - 1));
-// 		i += letter;
-// 	}
-// }
+	int	letter;
+
+	for (int i = 0; raw[i] != end; ++i) {
+		letter = 0;
+		while (raw[i + letter] != '\n' && raw[i + letter - 1] != '\r')
+			letter++;
+		strs.push_back(raw.substr(i, letter - 1));
+		i += letter;
+	}
+	return strs;
+}
+
+bool ft_if_method_implemented(std::string method) {
+
+	// put here implemented methods:
+	return (method.compare("GET")
+			&& method.compare("POST")
+			&& method.compare("DELETE"));
+}
+
+int ft_set_method(HTTP_Request *req, std::string url, int end) {
+
+	int	i = 0;
+
+	// set method
+	for (; url[i] != ' ' && i < 7 && i < end; ++i)
+		req->method.push_back(url[i]);
+
+	std::string method = req->method;
+
+	// checking if method implemented
+	if (ft_if_method_implemented(method)) {
+		Logger::putMsg("Method is not implemented", FILE_WREQ, WREQ);
+		req->answ_code[0] = 5;
+		req->answ_code[0] = 1;
+		return 0;
+	}
+
+	// other checks if we need (use "method", return 0 if false)
+
+	return i;
+}
+
+int	ft_set_uri(HTTP_Request *req, std::string url, int end, int i) {
+
+	for (++i; url[i] != ' ' && i != end ; ++i)
+		req->uri.push_back(url[i]);
+
+	if (req->uri.size() == 0) {
+		Logger::putMsg("Request hasn\'t URI", FILE_WREQ, WREQ);
+		req->answ_code[0] = 4;
+		req->answ_code[0] = 0;
+		return 0;
+	}
+
+	return (i);
+}
+
+int	ft_set_version(HTTP_Request *req, std::string url, int end, int i) {
+
+	for (++i; i != end; ++i)
+		req->version.push_back(url[i]);
+
+	if (req->version.size() == 0) {
+		Logger::putMsg("Request hasn\'t HTTP version", FILE_WREQ, WREQ);
+		req->answ_code[0] = 4;
+		req->answ_code[0] = 0;
+		return 0;
+	}
+
+	if (req->version.compare("HTTP/1.1")) {
+		Logger::putMsg("Request has wrong HTTP version", FILE_WREQ, WREQ);
+		req->answ_code[0] = 5;
+		req->answ_code[0] = 5;
+		return 0;
+	}
+
+	return i;
+}
+
+int	ft_set_url(HTTP_Request *req, std::string url) {
+
+	int	end = url.size() - 1;
+
+	int i = ft_set_method(req, url, end);
+
+	if (i > 0)
+		i = ft_set_uri(req, url, end, i);
+
+	if (i > 0)
+		i = ft_set_version(req, url, end, i);
+
+	return (i);
+}
+
+int	ft_make_hdr(HTTP_Request *req, std::string raw) {
+
+	int	i = 0;
+	int	end = raw.size() - 1;
+
+
+
+	std::string	key;
+	std::string	value;
+
+// Key
+	for (; i != end && raw[i] != ':'; ++i) {
+		key.push_back(raw[i]);
+	}
+
+	if (i == end) {
+		Logger::putMsg("Header " + key + "Doesn't have a value", FILE_WREQ, WREQ);
+		req->answ_code[0] = 4;
+		req->answ_code[0] = 0;
+		return 0;
+	}
+
+	if (req->headers.find(key) != req->headers.end()){
+		Logger::putMsg("Multiple request's header: " + key, FILE_WREQ, WREQ);
+		req->answ_code[0] = 4;
+		req->answ_code[0] = 0;
+		return 0;
+	}
+
+// Value
+	for (++i; raw[i] == ' '; ++i) {}
+
+	for (; i != end && raw[i] != '\n'; ++i)
+		if (raw[i] != '\r')
+			value.push_back(raw[i]);
+
+	std::pair<std::string, std::string> hdr = std::make_pair(key, value);
+	key.clear();
+	value.clear();
+
+	req->headers.insert(hdr);
+
+	return 1;
+}
+
+bool ft_set_hdrs(HTTP_Request *req, std::vector<std::string> req_str_arr, int end) {
+
+	int i = 1;
+
+// Heasers
+	for (; req_str_arr[i].compare("\r") && i != end; ++i)
+		if (ft_make_hdr(req, req_str_arr[i]))
+
+
+	if (req->headers.find("Host") == req->headers.end()) {
+		Logger::putMsg("Request hasn't \"Host\" header", FILE_WREQ, WREQ);
+		req->answ_code[0] = 4;
+		req->answ_code[0] = 0;
+		return 0;
+	}
+
+	return i;
+}
+
+void ft_set_body(HTTP_Request *req, std::vector<std::string> req_str_arr, int i, int end, int limit) {
+
+	int total_size = 0;
+
+	for (; req_str_arr[i] != req_str_arr[end]; ++i) {
+		total_size += req_str_arr[i].size();
+
+		if (total_size > limit) {
+			Logger::putMsg("Request\'s body is too large", FILE_WREQ, WREQ);
+			req->answ_code[0] = 4;
+			req->answ_code[0] = 14;
+			return ;
+		}
+
+		req->body.append(req_str_arr[i]);
+	}
+}
 
 void HTTP_Request::ft_strtoreq(Server &srv, HTTP_Request *req) {
 
-// URL
+	std::string	raw = srv.getReq();
 
-	int i = 0;
-	std::string raw = srv.getReq();
-	int end = raw.size() - 1;
+	std::vector<std::string> req_str_arr;
 
-//	ft_tokenisation()
-	// Method
-	for (; raw[i] != ' ' && i != end && i < 15; ++i)
-		req->method.push_back(raw[i]);
+// Request to sthrings vector
+	req_str_arr = ft_tokenisation(raw);
 
-	if (req->method.size() == 0) {
-		Logger::putMsg("Request hasn\'t method", FILE_WREQ, WREQ);
-		req->answ_code[0] = 4;
-		req->answ_code[0] = 0;
+// URL (Method, URI, HTTP-version)
+	if (!ft_set_url(req, req_str_arr[0]))
 		return ;
-	}
-	
-	// URI
-	for (++i; raw[i] != ' ' && i != end ; ++i) {
-		req->uri.push_back(raw[i]);
-	}
-	
-	if (req->uri.size() == 0) {
-		Logger::putMsg("Request hasn\'t URI" + raw, FILE_WREQ, WREQ);
-		req->answ_code[0] = 4;
-		req->answ_code[0] = 0;
-		return ;
-	}
-	
-	// Version
-	for (++i; raw[i] != '\n'  && i != end; ++i) {
-		if (raw[i] != '\r')
-			req->version.push_back(raw[i]);
-	}
 
-	if (req->version.size() == 0) {
-		Logger::putMsg("Request hasn\'t HTML version", FILE_WREQ, WREQ);
-		req->answ_code[0] = 4;
-		req->answ_code[0] = 0;
+// Headers
+	int	end = req_str_arr.size() - 1;
+	int i = ft_set_hdrs(req, req_str_arr, end);
+	if (!i)
 		return ;
-	}
-
-// Headers and Body				HTTP_Utils.hpp
-	if ((raw.size() - i) > srv.serv->limitCLientBodySize) {
-		req->answ_code[0] = 4;
-		req->answ_code[0] = 14;
-		return ;
-	}
-	req->answ_code[0] = ft_strtohdrs(raw, i, end, &(req->headers), &(req->body));
-	req->answ_code[1] = req->answ_code[0] % 100;
-	req->answ_code[0] = req->answ_code[0] / 100;
+		
+// Body
+	ft_set_body(req, req_str_arr, i, end, srv.serv->limitCLientBodySize);
 }
