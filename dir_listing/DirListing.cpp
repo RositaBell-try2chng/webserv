5189b9e3e0a72110077556353e4a8a25cb10dedb
Sprouts Coach <scoach@c2r11s5.42yerevan.am>
2023-06-21 18:52:32 +0400

    Dir listing guide done. Sorry, probably you will make it alone,\n hope this stuff will make it at least a bit easier

--- dir_listing/DirListing.cpp
+++ dir_listing/DirListing.cpp
@@ -6,15 +6,6 @@
 
 #include "DirListing.hpp"
 
-const char	*ft_make_name(const char *dir_nm, char *f_name) {
-
-	std::string full = dir_nm;
-	full.push_back('/');
-	full += f_name;
-	std::cout << full << std::endl;
-	return full.c_str();
-}
-
 int ft_dir_out(std::string dir_nm_str) {
 
 	const char *dir_nm = dir_nm_str.c_str();
@@ -30,26 +21,50 @@ int ft_dir_out(std::string dir_nm_str) {
 
 	DIR			*dir;
 	dirent		*current;
-	struct stat	*info = NULL;
+	struct stat	info;
 	
 	dir = opendir(dir_nm);
 
 	for (current = readdir(dir); current; current = readdir(dir)) {
-		std::cout << current->d_ino << "	" << current->d_name << std::endl;
-		if (stat(ft_make_name(dir_nm, current->d_name), info) == -1) {
+		std::cout << current->d_ino << "	" << current->d_name << '\n' << std::endl;
+
+		if (stat(current->d_name, &info) == -1) {
 			Logger::putMsg("Can't do \"stat()\"", FILE_ERR, ERR);
-			//return 1;
+			return 1;
 		}
 		else {
-			std::cout << info->st_atimespec.tv_nsec << std::endl;
+			std::cout << "st_atimespec.tv_nsec:		" << info.st_atimespec.tv_nsec << std::endl;
+			std::cout << "st_atimespec.tv_sec:		" << info.st_atimespec.tv_sec << std::endl;
+			std::cout << "st_birthtimespec.tv_nsec:	" << info.st_birthtimespec.tv_nsec << std::endl;
+			std::cout << "st_birthtimespec.tv_sec:	" << info.st_birthtimespec.tv_sec << std::endl;
+			std::cout << "st_blksize:			" << info.st_blksize << std::endl;
+			std::cout << "st_blocks:			" << info.st_blocks << std::endl;
+			std::cout << "st_ctimespec.tv_nsec:		" << info.st_ctimespec.tv_nsec << std::endl;
+			std::cout << "st_ctimespec.tv_sec:		" << info.st_ctimespec.tv_sec << std::endl;
+			std::cout << "st_dev:				" << info.st_dev << std::endl;
+			std::cout << "st_flags:			" << info.st_flags << std::endl;
+			std::cout << "st_gen:				" << info.st_gen << std::endl;
+			std::cout << "st_gid:				" << info.st_gid << std::endl;
+			std::cout << "st_ino:				" << info.st_ino << std::endl;
+			std::cout << "st_lspare:			" << info.st_lspare << std::endl;
+			std::cout << "st_mode:			" << info.st_mode << std::endl;
+			std::cout << "st_mtimespec.tv_nsec:		" << info.st_mtimespec.tv_nsec << std::endl;
+			std::cout << "st_mtimespec.tv_sec:		" << info.st_mtimespec.tv_sec << std::endl;
+			std::cout << "st_nlink:			" << info.st_nlink << std::endl;
+			std::cout << "st_qspare:			" << info.st_qspare << std::endl;
+			std::cout << "st_rdev:			" << info.st_rdev << std::endl;
+			std::cout << "st_size:			" << info.st_size << std::endl;
+			std::cout << "st_uid:				" << info.st_uid << std::endl;
 		}
+
 		std::cout << "	Length of filename:	" << current->d_namlen << std::endl;
 		std::cout << "	Length of record:	" << current->d_reclen << std::endl;
-		std::cout << "	seekof:		" << current->d_seekoff << std::endl;
+		std::cout << "	seekof:			" << current->d_seekoff << std::endl;
 		std::cout << "	Type of file:		" << static_cast<size_t>(current->d_type) << std::endl;
 		std::cout << "=======================================================" << std::endl;
 	}
 	
 	closedir(dir);
+
 	return 0;
 }
